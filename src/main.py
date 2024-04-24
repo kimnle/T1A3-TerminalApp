4b375b44aa735cd270706f6103e1c06b76d17739
kimnle <kimnle002@gmail.com>
2024-04-24 17:24:40 +1000

    exception handled if player name empty

--- src/main.py
+++ src/main.py
@@ -4,5 +4,11 @@ def game_title():
     print("Guess the number!!")
 
 def player_name():
-    player_name = input("Enter name: ")
-    print(f"Hey {player_name}")
+    try:
+        player_name = input("Player name: ")
+        if not player_name:
+            raise ValueError("Cannot be empty")
+        else:
+            print(f"Hey {player_name}")
+    except ValueError as e:
+            print(e)
