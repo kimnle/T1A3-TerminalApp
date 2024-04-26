2d20368d806c727b87a1b8903b3fb16c7158b1db
kimnle <kimnle002@gmail.com>
2024-04-26 16:10:17 +1000

    Removed player name exception handling

--- src/main.py
+++ src/main.py
@@ -4,14 +4,8 @@ def game_title():
     print("Guess the number!!")
 
 def player_name():
-    try:
-        name = input("Player name: ")
-        if not name:
-            raise ValueError("Cannot be empty")
-        else:
-            print(f"Hey {name}")
-    except ValueError as e:
-            print(e)
+    name = input("Player name: ")
+    print(f"Hey {name}")
 
 def game_level():
     level = input("Choose level - E for Easy, M for Medium or H or Hard: ")
